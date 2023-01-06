5dc948a33e404b6217d8f889c41da82dd3e95f28
Nguyen Smile <nguyenlc.smile@gmail.com>
2023-01-06 21:29:53 +0700

    update-data-wm

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -3,67 +3,62 @@ import { Store, State } from '@ngrx/store';
 import { AppState } from 'src/store/store.action';
 
 @Component({
-  selector: 'app-transactions',
-  templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.scss']
+    selector: 'app-transactions',
+    templateUrl: './transactions.component.html',
+    styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent implements OnInit {
-  valueMonitor: any;
-  updateMonitor: any;
-  dataWeek: any;
-  dataMonth: any;
-  dataDaily: any;
+    valueMonitor: any;
+    updateMonitor: any;
+    dataWeek: any;
+    dataMonth: any;
+    dataDaily: any;
 
-  transactionNapas: any = {
-    daily: {
-      total: 0,
-      success: 0,
-      fail: 0,
-    },
-    week: {
-      total: 0,
-      success: 0,
-      fail: 0,
-    },
-    month: {
-      total: 0,
-      success: 0,
-      fail: 0,
+    transactionNapas: any = {
+        week: {
+            total: 0,
+            success: 0,
+            fail: 0,
+        },
+        month: {
+            total: 0,
+            success: 0,
+            fail: 0,
+        }
     }
-  }
-  constructor(
-    private store: Store<AppState>
-  ) { }
+    constructor(
+        private store: Store<AppState>
+    ) { }
 
-  ngOnInit() {}
+    ngOnInit() { }
 
-  ngDoCheck() {
-    this.getValueMonitor();
-    this.getValueFunctionals(this.valueMonitor);
+    ngDoCheck() {
+        this.getValueMonitor();
+        this.getValueFunctionals(this.valueMonitor);
 
-    if (this.dataWeek) {
-      Object.keys(this.dataWeek).map(keyName => {
-        if (keyName === 'transaction_napas') this.transactionNapas.week = this.dataWeek.transaction_napas;
-      })
+        if (this.dataWeek) {
+            Object.keys(this.dataWeek).map(keyName => {
+                if (keyName === 'transaction_napas') this.transactionNapas.week = this.dataWeek.transaction_napas;
+            })
+        }
+        if (this.dataMonth) {
+            Object.keys(this.dataMonth).map(keyName => {
+                if (keyName === 'transaction_napas') this.transactionNapas.month = this.dataMonth.transaction_napas;
+            })
+        }
     }
-    if (this.dataMonth) {
-      Object.keys(this.dataMonth).map(keyName => {
-        if (keyName === 'transaction_napas') this.transactionNapas.month = this.dataMonth.transaction_napas;
-      })
+    getValueFunctionals(data) {
+        if (data[0]) {
+            this.dataWeek = data[0].week;
+            this.dataMonth = data[0].month;
+            this.dataDaily = data[0].daily;
+        }
     }
-  }
-  getValueFunctionals(data) {
-    if (data[0]) {
-      this.dataWeek = data[0].week;
-      this.dataMonth = data[0].month;
-      this.dataDaily = data[0].daily;
-    }
-  }
 
-  getValueMonitor() {
-    this.store.select(state => state.informationdata).subscribe(
-      getdata => this.valueMonitor = getdata
-    );
-  }
+    getValueMonitor() {
+        this.store.select(state => state.informationdata).subscribe(
+            getdata => this.valueMonitor = getdata
+        );
+    }
 
 }
--- src/store/store.reducer.ts
+++ src/store/store.reducer.ts
@@ -4,6 +4,9 @@ import { OnBoarding } from './information';
 export function addInformationReducer(state: OnBoarding[] = [], action) {
     switch (action.type) {
         case 'Monitor':
+            if (action.payload.daily !== undefined) {
+                return [action.payload];
+            }
             return [...state, action.payload];
         default:
             return state;
